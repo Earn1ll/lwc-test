b841600e077c94ae074c2f037c989918543b2aea
Earn1ll <coldsteelblr@gmail.com>
2022-03-15 12:28:23 +0300

    add contact table

--- .history/force-app/main/default/lwc/test/test_20220315115958.js
+++ .history/force-app/main/default/lwc/test/test_20220315115958.js
@@ -1,6 +1,6 @@
-import { LightningElement,wire } from 'lwc';
+import { api, LightningElement,api } from 'lwc';
 import getUsers from '../../classes/userController';
 
 export default class Test extends LightningElement {
-    @wire(getUsers) users;
+    @api
 }
